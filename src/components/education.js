75cc39e49dcabc2a8cca4f1320a6afb17262f4d0
Mitesh Patil <127524455+mitpatil07@users.noreply.github.com>
2025-09-22 09:22:22 +0530

    Update education.js

--- src/components/education.js
+++ src/components/education.js
@@ -120,7 +120,7 @@ const EducationalResources = () => {
   const [bookmarkedCourses, setBookmarkedCourses] = useState(new Set());
   const [thumbnails, setThumbnails] = useState({});
   const [filter, setFilter] = useState('all');
-  const visitorCount = 89;
+  const visitorCount = 206;
 
   useEffect(() => {
     COURSES_DATA.forEach(course => {
