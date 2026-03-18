dca737a1a6de09e90c393e9f42f19bb7b6d0a08d
mitpatil07 <mitesh8767@gmail.com>
2026-03-18 09:36:20 +0530

    fix cors preflight

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 app.use(express.json({ limit: '10kb' })); // Limit body payload to 10kb
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
